953c1b58eda5aeec7cbad54644409ecb2c5b19fa
mohammedtigrini1 <mohammed.tigrini@appdirect.com>
2022-11-28 09:24:40 -0500

    Stuff.

--- routes/health.js
+++ routes/health.js
@@ -1,5 +1,12 @@
 const router = require("express").Router();
 
+router.get("/", async (req, res, next) => {
+    res.json({
+        success: true, 
+        message: "Server is running."
+    });
+})
+
 router.get('/health', async (req, res, next) => {
     res.json({
         success: true,
