0889c8fd152accac1420547f6d328cb9ff6fbacf
mohammedtigrini1 <mohammed.tigrini@appdirect.com>
2022-11-28 10:18:36 -0500

    Skeleton.

--- routes/shape.js
+++ routes/shape.js
@@ -10,9 +10,28 @@ router.get('/shapes', checkJWT, async (req, res, next) => {
     });
 });
 
-
-router.get('/shapes', checkJWT, async (req, res, next) => {
-    //
+router.get('/shape/:name', checkJWT, async (req, res, next) => {
+    // TODO
+    res.json({
+        success: true, 
+        message: `Successfully fetched shape ${req.params.name}`
+    })
 });
+
+router.get('/shape/:id', async (req, res, next) => {
+    // TOOD
+    res.json({
+        success: true,
+        message: `Successfully fetched shape ${req.params.id}`
+    });
+});
+
+router.post('/shape', async (req, res, next) => {
+    // TODO
+    res.json({
+        success: true,
+        message: `Successfully created shape`
+    });
+})
 
 module.exports = router;
