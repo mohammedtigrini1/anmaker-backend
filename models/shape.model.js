e361c830b62b0ebf187ddc1f34488a5e6544812c
mohammedtigrini1 <mohammed.tigrini@appdirect.com>
2022-12-01 22:21:34 -0500

    Fetching shapes.

--- models/shape.model.js
+++ models/shape.model.js
@@ -1,10 +1,27 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const CoordinatesSchema = new Schema({
+  x: { type: number, required: true },
+  y: { type: number, required: true },
+});
+
+const ShapeInfoSchema = new Schema({
+  name: {
+    type: String,
+    enum: ["rectangle", "triangle", "text", "circle", "composite"],
+  },
+  height: { type: String },
+  width: { type: String },
+  font: { type: String },
+  text: { type: String },
+  color: { type: String },
+  coordinates: { type: CoordinatesSchema },
+});
+
 const ShapeSchema = new Schema({
-  website1: { type: mongoose.Types.ObjectId, unique: true, required: true },
-  website2:{ type: mongoose.Types.ObjectId, unique: true, required: true },
-  created: { type: Date, default: Date.now() }
+  shapeName: { type: String, unique: true, required: true },
+  shapeInfo: [ShapeInfoSchema],
 });
 
 module.exports = mongoose.model("shape", ShapeSchema, "shapes");
--- routes/shape.js
+++ routes/shape.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { checkJWT } = require("../middlewares/check-jwt");
-const { schema } = require("../models/shape.model");
+const ShapeModel = require("../models/shape.model");
 
 router.get('/shapes', checkJWT, async (req, res, next) => {
     // TODO
@@ -11,11 +11,28 @@ router.get('/shapes', checkJWT, async (req, res, next) => {
 });
 
 router.get('/shape/:name', checkJWT, async (req, res, next) => {
-    // TODO
-    res.json({
-        success: true, 
-        message: `Successfully fetched shape ${req.params.name}`
-    })
+    ShapeModel.find({shapeName : req.params.name}, (err, shape) => {
+        if(err) {
+            res.status(500).json({
+                success: false,
+                message: `Failed to get shape ${req.params.name}.`
+            });
+            return;
+        }
+
+        if(!shape) {
+            res.status(404).json({
+                success: false,
+                message: `Shape does not exist`
+            });
+            return;
+        }
+        res.json({
+            success: true,
+            message: "Successfully fetched shape.",
+            shape: shape
+        });
+    });
 });
 
 router.get('/shape/:id', async (req, res, next) => {
