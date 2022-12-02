ffa343a0724528aa3f879b7acb967a3dd27d3aa8
mohammedtigrini1 <mohammed.tigrini@appdirect.com>
2022-12-01 22:52:50 -0500

    Format document.

--- routes/shape.js
+++ routes/shape.js
@@ -2,53 +2,53 @@ const router = require("express").Router();
 const { checkJWT } = require("../middlewares/check-jwt");
 const ShapeModel = require("../models/shape.model");
 
-router.get('/shapes', checkJWT, async (req, res, next) => {
-    // TODO
-    res.json({
-        success: true,
-        message: "Successfully fetched shape."
-    });
+router.get("/shapes", checkJWT, async (req, res, next) => {
+  // TODO
+  res.json({
+    success: true,
+    message: "Successfully fetched shape.",
+  });
 });
 
-router.get('/shape/:name', checkJWT, async (req, res, next) => {
-    ShapeModel.find({shapeName : req.params.name}, (err, shape) => {
-        if(err) {
-            res.status(500).json({
-                success: false,
-                message: `Failed to get shape ${req.params.name}.`
-            });
-            return;
-        }
+router.get("/shape/:name", checkJWT, async (req, res, next) => {
+  ShapeModel.find({ shapeName: req.params.name }, (err, shape) => {
+    if (err) {
+      res.status(500).json({
+        success: false,
+        message: `Failed to get shape ${req.params.name}.`,
+      });
+      return;
+    }
 
-        if(!shape) {
-            res.status(404).json({
-                success: false,
-                message: `Shape does not exist`
-            });
-            return;
-        }
-        res.json({
-            success: true,
-            message: "Successfully fetched shape.",
-            shape: shape
-        });
+    if (!shape) {
+      res.status(404).json({
+        success: false,
+        message: `Shape does not exist`,
+      });
+      return;
+    }
+    res.json({
+      success: true,
+      message: "Successfully fetched shape.",
+      shape: shape,
     });
+  });
 });
 
-router.get('/shape/:id', async (req, res, next) => {
-    // TOOD
-    res.json({
-        success: true,
-        message: `Successfully fetched shape ${req.params.id}`
-    });
+router.get("/shape/:id", async (req, res, next) => {
+  // TOOD
+  res.json({
+    success: true,
+    message: `Successfully fetched shape ${req.params.id}`,
+  });
 });
 
-router.post('/shape', async (req, res, next) => {
-    // TODO
-    res.json({
-        success: true,
-        message: `Successfully created shape`
-    });
-})
+router.post("/shape", async (req, res, next) => {
+  // TODO
+  res.json({
+    success: true,
+    message: `Successfully created shape`,
+  });
+});
 
 module.exports = router;
